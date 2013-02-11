a85399c83c8b851714a6654066b27b25a4c97f87
roland <roland@javalia.se>
2013-02-11 20:16:14 +0100

    Initial

---
+++ project.clj
@@ -0,0 +1,10 @@
+(defproject hello-world "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [compojure "1.1.5"]
+                 [hiccup "1.0.2"]]
+  :plugins [[lein-ring "0.8.2"]]
+  :ring {:handler hello-world.handler/app}
+  :profiles
+  {:dev {:dependencies [[ring-mock "0.1.3"]]}})
---
+++ src/hello_world/handler.clj
@@ -0,0 +1,66 @@
+(ns hello-world.handler
+  (:use [compojure.core]
+        [hiccup.core]
+        [hiccup.page]
+        [hiccup.form]
+        [hiccup.element]
+        [hiccup.def])
+  (:require [compojure.handler :as handler]
+            [compojure.route :as route]
+      ))
+
+(defn parse-int [s]
+   (Integer. (re-find  #"\d+" s )))
+
+(defn add-form [x y]
+  (form-to [:post "/calc"]
+        [:div
+         [:ul
+          [:li "Ett "
+           [:input {:type "text"
+           :name  "add1"
+           :id "add1"
+           :value x}]]
+          [:li "Två "
+           [:input {:type "text"
+           :name "add2"
+           :id "add2"
+           :value y}]]]
+         [:input {:type "submit"
+                :value "Summera"}]]))
+
+
+
+(defn start-page []
+  (html 
+   (html5
+      [:html
+       [:head
+        [:title "Leker med clojure"]]
+       [:body
+        [:div
+         [:h2 "Vi testar att räkna lite"]]
+        (link-to "/calc" "Addera lite")
+        ]])))
+
+(defn add-page [params] 
+  (html 
+   (html5
+      [:html
+       [:head
+        [:title "Leker med clojure"]]
+       [:body
+        [:div
+         [:h2 "Vi räknar lite"]]
+        [:div (+ (parse-int(params :add1)) (parse-int(params :add2)))]
+        (add-form (params :add1) (params :add2))
+        ]])))
+
+(defroutes app-routes
+  (GET "/" [] (start-page))
+  (GET "/calc" [] (add-page {:add1 "0" :add2 "0"}))
+  (POST "/calc" {params :params} (add-page params))
+  (route/not-found "Not Found"))
+
+(def app
+  (handler/site app-routes))
